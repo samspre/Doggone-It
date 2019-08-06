3ca86fc4df478c6fc851609e82f751194bc6981e
KristoferKwan <35852328+KristoferKwan@users.noreply.github.com>
2019-08-06 16:06:23 -0400

    ADD: image PIL endpoint

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 import os
+import sys
 import jinja2
-from flask import Flask, render_template, redirect, session, request
-#from models.Setting import Setting
+from flask import Flask, render_template, redirect, session, request, flash, url_for
 import logging
 from forms.dogImage import DogImage 
+from werkzeug.utils import secure_filename
+from PIL import Image
 
-current_directory = jinja2.Environment(loader=jinja2.FileSystemLoader(
-os.path.dirname(__file__)), extensions=['jinja2.ext.autoescape'], autoescape=True)
-app = Flask(__name__)
+UPLOAD_FOLDER = 'static/images/image_input'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+dir_path = os.path.dirname(os.path.realpath(__file__))
+app = Flask(__name__, instance_path=dir_path)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+"""
 
+function eval_image takes the user inputted image and reads it utilizing the PIL module
+image_path leads to the static/images/image_input folder in the flask app directory 
+Note: in order to print items in flask, you need to flush the buffer, or redirect the file via sys.stderr instead of stdout
+    usage: print(<message>, file=sys.stderr)
+"""
+def eval_image(image_path):
+    im = Image.open(image_path)
+    #im.show()
+    print("not implemented yet")
 
-@app.route('/', methods = ["GET"])
+@app.route('/results', methods = ["GET"]) #results page endpoint
+def results():
+    return render_template('results.html')
+
+@app.route('/', methods = ["GET", "POST"]) #initial homepage
 def home():
-    dog_image = DogImage(request.form)
-    #print(assets.url)
-    # return redirect("/login", code=302)
-    #logging.info("testingadjfladjflkdsajfiefjalsfslkfjeaifjealfekajfakdsfijewaf")
-    return render_template("home.html", form=dog_image)
+    form = DogImage()
+    if  request.method == "POST" and form.validate_on_submit(): #reads from the frontend the form image
+        image = form.image.data
+        filename = secure_filename(image.filename)
+        image_path = os.path.join(app.instance_path, app.config['UPLOAD_FOLDER'], filename)
+        image.save(image_path) #saves image to specified image path
+        eval_image(image)
+        return redirect(url_for('results')) #redirects the user to a "results" page (currently nothings in it)
+    return render_template("home.html", form=form)
 
 if __name__ == '__main__':
     app.secret_key = 'secret1234'
