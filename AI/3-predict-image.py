0f5b38eb5b7ad3ecb3953548ca5e4c91b27412a8
barnesv17 <50973809+barnesv17@users.noreply.github.com>
2019-08-11 19:46:18 -0300

    Add files via upload

---
+++ AI/3-predict-image.py
@@ -0,0 +1,28 @@
+import cv2
+import tensorflow as tf
+
+CATEGORIES = [ "pug", "husky" ]
+
+def prepare(filepath):
+    IMG_SIZE = 90
+    img_array = cv2.imread(filepath)
+    new_array = cv2.resize( img_array, (IMG_SIZE,IMG_SIZE) )
+    return new_array.reshape( -1, IMG_SIZE, IMG_SIZE, 3 )
+
+model = tf.keras.models.load_model("trained.model") # Load the trained model from an outside file
+
+'''
+Below are examples of testing a single image on our trained model.
+TO DO:
+Take the image imputted by the user in our GUI.
+Predict that image using this model.
+Return the prediction result to the user in our GUI.
+'''
+prediction = model.predict([prepare('pug_test.png')])
+print( CATEGORIES[int(prediction[0][0])] )
+
+prediction = model.predict([prepare('husky_test.png')])
+print( CATEGORIES[int(prediction[0][0])] )
+
+prediction = model.predict([prepare('pug_test2.png')])
+print( CATEGORIES[int(prediction[0][0])] )
